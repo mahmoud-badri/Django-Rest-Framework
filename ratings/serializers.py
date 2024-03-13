62d74d38a97c3bf6085938119265af8801942044
AhmedMEladawy <ahmedeladawy.m@gmail.com>
2024-03-13 13:14:44 +0200

    Added Ratings API

---
+++ ratings/serializers.py
@@ -0,0 +1,8 @@
+# ratings/serializers.py
+from rest_framework import serializers
+from .models import Rating
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = '__all__' 
