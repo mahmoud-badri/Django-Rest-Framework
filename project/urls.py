e19a2d4de127e5b7599db2dd102c58217355c2a2
AyaElgedawy <ayaelgedawy4@gmail.com>
2024-03-13 08:29:48 +0200

    add appointment api

--- project/urls.py
+++ project/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path ,include
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
-    path('hotel/', include('hotel.urls'))
+    path('hotel/', include('hotel.urls')),
+    path('appoint/', include('appointments.urls')),
+
 ]
