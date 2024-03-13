79871ed436cefc395ea5e968c3fdcf100217ad62
AyaElgedawy <ayaelgedawy4@gmail.com>
2024-03-13 09:39:30 +0200

    hotel model

--- hotel/models.py
+++ hotel/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 class Hotel(models.Model):
     name = models.CharField(max_length=100)
-    
+    image = models.ImageField(upload_to='hotel_images/')
     def __str__(self):
         return self.name
--- project/urls.py
+++ project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path ,include
+from django.conf.urls.static import static
+from .settings import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hotel/', include('hotel.urls'))
-]
+]+ static(MEDIA_URL,document_root=MEDIA_ROOT)
