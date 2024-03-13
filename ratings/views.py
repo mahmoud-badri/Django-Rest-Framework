7d12039e761bcfdc713265f3a791134ae1794440
AhmedMEladawy <ahmedeladawy.m@gmail.com>
2024-03-13 04:03:45 +0200

    Added Ratings API

---
+++ ratings/views.py
@@ -0,0 +1,8 @@
+# ratings/views.py
+from rest_framework import viewsets
+from .models import Rating
+from .serializers import RatingSerializer
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
